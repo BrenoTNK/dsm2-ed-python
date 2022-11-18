a2344a2e1288df4f1134c709011a87ef77df2a92
BrenoTNK <70177109+BrenoTNK@users.noreply.github.com>
2022-11-18 20:39:33 -0300

    Aula de Estrutura de Dados 18.11

--- 18_lista_duplamente_encadeada.py
+++ 18_lista_duplamente_encadeada.py
@@ -18,6 +18,36 @@ print(lista)
 lista.insert(3, '2 kg batata')
 print(lista)
 
+# Inserção intermediária na posição 5
+lista.insert(5, '1 cx caldo de galinha')
+print(lista)
+
 # Inserção em posição não existente, >= count
 lista.insert(15, '5 kg arroz')
+print(lista)
+
+# Consulta de itens da lista
+primeiro = lista.peek_front()
+ultimo = lista.peek_back()
+item_pos6 = lista.peek(6)
+print(f'Primeiro: {primeiro}; Último: {ultimo}; Pos 6: {item_pos6}')
+
+# Remoção do primeiro item
+primeiro = lista.remove_front()
+print(f'REMOVIDO, INICIAL: {primeiro}')
+print(lista)
+
+# Remoção do último item
+ultimo = lista.remove_back()
+print(f'REMOVIDO, FINAL: {ultimo}')
+print(lista)
+
+# Remoção em posição insermediária (3)
+item_pos3 = lista.remove(3)
+print(f'REMOVIDO, POS 3: {item_pos3}')
+print(lista)
+
+# Remoção em posição insermediária (1)
+item_pos1 = lista.remove(1)
+print(f'REMOVIDO, POS 1: {item_pos1}')
 print(lista)
